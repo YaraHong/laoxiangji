1cb375665771d9a4693d47218b7532df8ea857ba
try_except_me <18213823950@163.com>
2026-05-17 14:20:05 +0800

    bug修复

--- backend/app/core/model_factory.py
+++ backend/app/core/model_factory.py
@@ -1,44 +1,20 @@
-from langchain_openai import ChatOpenAI, OpenAI
+from langchain_community.chat_models import ChatTongyi
+from openai import OpenAI
 
-from app.core.config import settings
-
-chat_llm = ChatOpenAI(
-    model=settings.models.chat.model_name,
-    base_url=settings.models.chat.base_url,
-    api_key=settings.models.chat.api_key,
-    temperature=0.7,
-    max_tokens=2000,
-    timeout=300
+chat_llm = ChatTongyi(
+    model="deepseek-v4-pro"
 )
 
-streaming_chat_llm = ChatOpenAI(
-    model=settings.models.chat.model_name,
-    base_url=settings.models.chat.base_url,
-    api_key=settings.models.chat.api_key,
-    temperature=0.7,
-    max_tokens=2000,
-    timeout=300,
+streaming_chat_llm = ChatTongyi(
+    model="deepseek-v4-pro",
     streaming=True
 )
 
-intent_llm = ChatOpenAI(
-    model=settings.models.intent.model_name,
-    base_url=settings.models.intent.base_url,
-    api_key=settings.models.intent.api_key,
-    temperature=0.7,
-    max_tokens=2000,
-    timeout=300,
-    streaming=True
+intent_llm = ChatTongyi(
+    model="deepseek-v4-flash"
 )
 
-client = OpenAI(
+openai_client = OpenAI(
     base_url='https://api-inference.modelscope.cn/v1',
     api_key='ms-62c5b98c-6b4b-4378-acf2-c70c4196c7b9',
-)
-
-embedding_model = client.embeddings.create(
-    model='Qwen/Qwen3-Embedding-8B',
-    input='你好',
-    encoding_format="float",
-    dimensions=1024
 )
